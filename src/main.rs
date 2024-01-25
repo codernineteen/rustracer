689cb4a0c82ba6eea19d9c2573869314932c317a
codernineteen <wjsdPcks34@gmail.com>
2024-01-25 17:27:59 +0900

    Test camera usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use raytracer::components::camera::{self, Camera};
 fn main() {
-    println!("Hello, world!");
+    let image_width = 400;
+    let aspect_ratio = 16.0 / 9.0;
+    let samples_per_pixel = 100;
+    let depth = 10;
+
+    let camera = Camera::new(image_width, aspect_ratio, samples_per_pixel, depth);
+    camera.write_color();
 }
