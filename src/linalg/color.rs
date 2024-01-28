05bdd62da508c391b2de3e452f53266e4e298ee0
codernineteen <wjsdPcks34@gmail.com>
2024-01-28 13:56:44 +0900

    update linear algebra sub modules

--- src/linalg/color.rs
+++ src/linalg/color.rs
@@ -1,5 +1,21 @@
 use crate::linalg::vector::Vector3;
+use image::{Rgb, RgbImage};
 
+/*
+    logical alias of Vector3
+    Fields follow this mapping :
+    x == r
+    y == g
+    z == b
+*/
 pub type Color3 = Vector3;
 
-pub fn write_color() {}
+pub fn write_color(img: &mut RgbImage, pixel: Color3, row: u32, col: u32) {
+    let pixel = Rgb([
+        (pixel.x * 255.999) as u8,
+        (pixel.y * 255.999) as u8,
+        (pixel.z * 255.999) as u8,
+    ]);
+
+    img.put_pixel(col, row, pixel); // width | height | pixel value
+}
--- src/linalg/vector.rs
+++ src/linalg/vector.rs
@@ -1,7 +1,7 @@
 use rand::{thread_rng, Rng};
 use std::ops::{Add, Div, Mul, Sub};
 
-#[derive(Debug, Default)]
+#[derive(Debug, Copy, Clone)]
 pub struct Vector3 {
     pub x: f64,
     pub y: f64,
@@ -32,7 +32,7 @@ impl Vector3 {
         }
     }
 
-    pub fn invert(self) -> Self {
+    pub fn invert(&self) -> Self {
         Self {
             x: 1.0 / self.x,
             y: 1.0 / self.y,
@@ -41,7 +41,7 @@ impl Vector3 {
     }
 
     pub fn dot(u: &Vector3, v: &Vector3) -> f64 {
-        u.x * v.y + u.y + v.y + u.z + v.z
+        (u.x * v.x) + (u.y * v.y) + (u.z * v.z)
     }
 
     pub fn cross(u: &Vector3, v: &Vector3) -> Self {
@@ -59,8 +59,25 @@ impl Vector3 {
     pub fn length(&self) -> f64 {
         Self::length_squared(&self).sqrt()
     }
+
+    pub fn normalize(&self) -> Self {
+        Vector3::new(self.x, self.y, self.z) / self.length()
+    }
 }
 
+impl Default for Vector3 {
+    fn default() -> Self {
+        Self {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    }
+}
+
+/*
+   impl block for Arithmetic operators
+*/
 impl Add<Vector3> for Vector3 {
     type Output = Vector3;
 
@@ -111,6 +128,18 @@ impl Mul<f64> for Vector3 {
     }
 }
 
+impl Mul<Vector3> for f64 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: Vector3) -> Self::Output {
+        Vector3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
 impl Div<Vector3> for Vector3 {
     type Output = Vector3;
 
@@ -123,6 +152,146 @@ impl Div<f64> for Vector3 {
     type Output = Vector3;
 
     fn div(self, rhs: f64) -> Self::Output {
+        assert_ne!(rhs, 0.0);
         self.mul(1.0 / rhs)
+    }
+}
+
+/*
+    Test suite for Vector3
+*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // arithmetic tests
+    #[test]
+    fn test_add() {
+        let v1 = Vector3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+        let v2 = Vector3 {
+            x: 4.0,
+            y: 5.0,
+            z: 6.0,
+        };
+        let result = v1 + v2;
+        assert_eq!(result.x, 5.0);
+        assert_eq!(result.y, 7.0);
+        assert_eq!(result.z, 9.0);
+    }
+
+    #[test]
+    fn test_sub() {
+        let v1 = Vector3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+        let v2 = Vector3 {
+            x: 4.0,
+            y: 5.0,
+            z: 6.0,
+        };
+        let result = v1 - v2;
+        assert_eq!(result.x, -3.0);
+        assert_eq!(result.y, -3.0);
+        assert_eq!(result.z, -3.0);
+    }
+
+    #[test]
+    fn test_mul() {
+        let v1 = Vector3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+        let v2 = Vector3 {
+            x: 4.0,
+            y: 5.0,
+            z: 6.0,
+        };
+        let result = v1 * v2;
+        let result2 = v2 * 2.0;
+        assert_eq!(result.x, 4.0);
+        assert_eq!(result.y, 10.0);
+        assert_eq!(result.z, 18.0);
+        assert_eq!(result2.x, 8.0);
+        assert_eq!(result2.y, 10.0);
+        assert_eq!(result2.z, 12.0);
+    }
+
+    #[test]
+    fn test_div() {
+        let v1 = Vector3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        };
+        let v2 = Vector3 {
+            x: 4.0,
+            y: 4.0,
+            z: 4.0,
+        };
+        let result = v1 / v2;
+        let result2 = v2 / 2.0;
+        assert_eq!(result.x, 0.25);
+        assert_eq!(result.y, 0.25);
+        assert_eq!(result.z, 0.25);
+        assert_eq!(result2.x, 2.0);
+        assert_eq!(result2.y, 2.0);
+        assert_eq!(result2.z, 2.0);
+    }
+
+    // vector operation tests
+    #[test]
+    fn test_dot() {
+        let v1 = Vector3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        };
+        let v2 = Vector3 {
+            x: 4.0,
+            y: 4.0,
+            z: 4.0,
+        };
+        let result = Vector3::dot(&v1, &v2);
+        assert_eq!(result, 12.0);
+    }
+
+    #[test]
+    fn test_unit() {
+        let v = Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 4.0,
+        };
+        let result = v.normalize();
+        assert_eq!(result.x, 0.0);
+        assert_eq!(result.y, 0.0);
+        assert_eq!(result.z, 1.0);
+    }
+
+    #[test]
+    fn test_length() {
+        let v = Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 4.0,
+        };
+
+        assert_eq!(v.length(), 4.0);
+        assert_eq!(v.length_squared(), 16.0);
+    }
+
+    #[test]
+    fn test_default() {
+        let v = Vector3::default();
+        assert_eq!(v.x, 0.0);
+        assert_eq!(v.y, 0.0);
+        assert_eq!(v.z, 0.0);
     }
 }
